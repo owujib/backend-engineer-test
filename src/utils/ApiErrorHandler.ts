57fd3b8f4ff24dde9a008d89567e256d6a4981bb
owujib <owujibfavour@gmail.com>
2024-03-26 12:51:21 +0100

    resolving render build errors

--- src/utils/ApiErrorHandler.ts
+++ src/utils/ApiErrorHandler.ts
@@ -7,9 +7,12 @@ export default class ApiExceptionHandler extends Error {
   constructor(message: string, statusCode?: number | null, error?: any) {
     super(message);
     this.statusCode = statusCode || 400;
+    // this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.status = false;
     this.error = error;
 
     this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
   }
 }
